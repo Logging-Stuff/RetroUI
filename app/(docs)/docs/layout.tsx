e442086b84e582f5b828f2f63c8a2550801a49a1
ariflogs <devarifhossain@gmail.com>
2024-09-29 06:27:13 +0600

    ✅ Good looking sidenav

---
+++ app/(docs)/docs/layout.tsx
@@ -0,0 +1,10 @@
+import SideNav from '@/components/SideNav'
+import React from 'react'
+
+export default function layout() {
+  return (
+    <div>
+        <SideNav />
+        layout</div>
+  )
+}
