34837d653fe86e11c3c2885e18092affef67b880
ariflogs <devarifhossain@gmail.com>
2024-10-01 06:10:35 +0600

    changed cta from newsletter to github and community

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -12,52 +12,46 @@ import {
   Input,
   Textarea,
 } from "@/packages/ui";
+import { GithubIcon, MessageCircle } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="space-y-36">
-      <section className="container max-w-6xl mx-auto mt-36 text-center text-gray-900">
-        <H1>Make your projects</H1>
-        <H1 className="text-outlined">stand out!</H1>
+      <section className="container max-w-6xl mx-auto mt-36 text-gray-900 flex justify-center items-center">
+        <div className="w-2/3">
+          <H1>Make your projects</H1>
+          <H1 className="text-outlined">stand out!</H1>
 
-        <p className="text-lg text-gray-600 mb-8 mt-4 max-w-2xl mx-auto">
-          Retro styled component library for modern web apps. Comes with 20+
-          free UI components that you can just copy paste into your projects.{" "}
-          {/* <br /> Now available for both HTML and React! */}
-        </p>
+          <p className="text-lg text-muted mb-8 mt-4">
+            Retro styled component library for modern web apps. Comes with 20+
+            free UI components that you can just copy paste into your projects.{" "}
+            {/* <br /> Now available for both HTML and React! */}
+          </p>
 
-        <div className="max-w-96 grid gap-4 mx-auto grid-cols-2">
-          <Link href="/docs" passHref>
-            <Button className="w-full" aria-label="Get Started with RetroUI">
-              Get Started
-            </Button>
-          </Link>
-          <Link
-            href="https://github.com/ariflogs/retroui"
-            target="_blank"
-            passHref
-          >
-            <Button
-              className="w-full"
-              variant="outline"
-              aria-label="Get Started with RetroUI"
+          <div className="max-w-96 grid gap-4 grid-cols-2">
+            <Link href="/docs" passHref>
+              <Button className="w-full" aria-label="Get Started with RetroUI">
+                Get Started
+              </Button>
+            </Link>
+            <Link
+              href="https://github.com/ariflogs/retroui"
+              target="_blank"
+              passHref
             >
-              Github
-            </Button>
-          </Link>
-        </div>
-      </section>
-
-      {/* Features Section */}
-      <section className="container max-w-6xl mx-auto ">
-        <div className="grid gird-cols-1 lg:grid-cols-2 items-center">
-          <div>
-            <H3>Why Use RetroUI?</H3>
-            <H1 className="mt-4">Coz We C000l!!</H1>
+              <Button
+                className="w-full"
+                variant="outline"
+                aria-label="Get Started with RetroUI"
+              >
+                Github
+              </Button>
+            </Link>
           </div>
-
+        </div>
+        <div className="w-1/3">
           <img
             alt="orange cool cat"
             src="/images/orange_boos.png"
@@ -66,9 +60,8 @@ export default function Home() {
         </div>
       </section>
 
-      {/* Component Previews Section */}
       <section className="container max-w-6xl mx-auto ">
-        <H2 className="mb-16">
+        <H2 className="mb-16 text-center">
           Our components look both old school and modern! ✨
         </H2>
 
@@ -110,7 +103,7 @@ export default function Home() {
       </section>
 
       <section className="container max-w-6xl mx-auto">
-        <H2 className="mb-16">
+        <H2 className="mb-16 text-center">
           And NO! We didn&apos;t just copy Gumroad!
           <br />
           Inspired, if you will... 💅
@@ -140,7 +133,30 @@ export default function Home() {
         </div>
       </section>
 
-      <JoinNewsletter />
+      <section className="container max-w-6xl mx-auto border-2 bg-primary-300 border-black p-16">
+        <H2 className="mb-8 text-center">
+          And did I mention it&apos;s Free and Open Source?
+        </H2>
+        <div className="flex justify-center space-x-4">
+          <Link
+            href="https://github.com/ariflogs/retroui"
+            target="_blank"
+            passHref
+          >
+            <Button className="flex items-center bg-white" variant="outline">
+              <GithubIcon size="16" className="mr-2" />
+              Star on GitHub
+            </Button>
+          </Link>
+          <Link href="https://discord.gg/Jum3NJxK6Q" target="_blank" passHref>
+            <Button className="flex items-center bg-white" variant="outline">
+              <MessageCircle size="16" className="mr-2" />
+              Join Community
+            </Button>
+          </Link>
+        </div>
+      </section>
+
       <footer className="bg-black py-8">
         <div className="container max-w-6xl mx-auto flex flex-col lg:flex-row space-y-4 lg:space-y-0 justify-between items-center">
           <div className="flex justify-center space-x-4">
