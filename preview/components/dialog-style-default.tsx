a06a34aad0c4beb0f21dff903da3d0518500a826
Allan Diego <allandiego252@gmail.com>
2024-10-26 15:20:10 -0300

    feat: add default dialog example

---
+++ preview/components/dialog-style-default.tsx
@@ -0,0 +1,34 @@
+import {
+  Button,
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTrigger,
+  Text,
+} from "@/packages/ui";
+
+export default function DialogStyleDefault() {
+  return (
+    <Dialog defaultOpen>
+      <DialogTrigger asChild>
+        <Button>Open Dialog</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <Text as="h5">Confirm your action?</Text>
+        </DialogHeader>
+        <section className="flex flex-col gap-4 p-4">
+          <section className="text-xl">
+            <p>Are you sure you want to delete this item?</p>
+            <p>This action cannout be undone.</p>
+          </section>
+          <section className="flex w-full justify-end">
+            <DialogTrigger asChild>
+              <Button>Confirm</Button>
+            </DialogTrigger>
+          </section>
+        </section>
+      </DialogContent>
+    </Dialog>
+  );
+}
