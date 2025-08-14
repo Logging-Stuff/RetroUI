9ecdca96b4c91952c2f243b0ea63e0259104b5a9
ariflogs <devarifhossain@gmail.com>
2025-08-15 02:17:08 +0600

    badge example wrapped

--- preview/components/badge-style-variants.tsx
+++ preview/components/badge-style-variants.tsx
@@ -2,7 +2,7 @@ import { Badge } from "@/components/retroui/Badge";
 
 export default function BadgeStyleVariants() {
   return (
-    <div className="space-x-4">
+    <div className="flex flex-wrap gap-4">
       <Badge>Default</Badge>
       <Badge variant="outline">Outlined</Badge>
       <Badge variant="solid">Solid</Badge>
